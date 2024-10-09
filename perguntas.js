773dc19024e75581a427b9f8ece58fb1fb928618
joaolucascomar <141441384+joaolucascomar@users.noreply.github.com>
2024-10-09 08:46:58 -0300

    uai

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a formula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'Bioquimica',
+    'Qual a polaridade da água?',
+    'Polar'
+)
+
+criaCartao(
+    'Quimica',
+    'O que são hidrocarbonetos',
+    'Compostos orgânicos formadoa apenas por átomos de carbono e hidrogênio'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a diferença entre dominância completa e codominância?',
+    'Na dominância completa o alelo dominante se sobrepõe sobre o alelo recessivo, na codominância ambos os alelos se expressam igualmente'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual o numero de carbonos de um composto alcino com 24 hidrogênios',
+    '13 carbonos'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Marechal Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'Nomeia todos os continentes:',
+    'América, Africa, Ásia, Europa, Oceania, Antartica'
+)
+
+criaCartao(
+    'História',
+    'Quantos anos durou a segunda guerra mundial?',
+    '6'
+)
+
+criaCartao(
+    'Geografia',
+    'Nomeie todos os oceanos?',
+    'Oceano Atlantico, Oceano Indico, Oceano Pacífico, Oceano Glacial Ártico, Oceano Glacial Antártico'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os 5 reinos dos seres vivos?',
+    'Monera, Proctista, Fungi, Plantae, Animalia'
+)
+
+criaCartao(
+    'Português',
+    'Quais são os momentos da literatura brasileira',
+    'Quinhentismo, Barroco, Arcadismo, Romantismo, Realismo, Naturalismo, Parnasianismo, Simbolismo, Modernismo e Pós-Modernismo'
+)
+
+criaCartao(
+    'Português',
+    'Qual a diferença entre gráfico e infogáfico',
+    'O grafico é uma representação de dados quantitativos e o infográfico combina texto, imagens e gráficos para apresentar as informações'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a formula da circunferência de um circulo',
+    'c = 2πr (onde r é o raio)'
+)
+
+criaCartao(
+    'Programação',
+    'Quais as três principais tecnologias do desenvolvimento web?',
+    'HTML, CSS e JS'
+)
+
+criaCartao(
+    'Matemática',
+    'Quais a razão trigonométrica básicas para calcular a tangente',
+    'tan = cateto oposto/cateto adjacente'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a formula geral da função afim',
+    'f(x) = ax + b'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um dado tipo booleano',
+    'Possui dois valores, verdadeiro ou falso'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o teorema de pitágoras',
+    'a² + b² = c²'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a formula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a formula: ((a1+an)*r)/2'
+)
