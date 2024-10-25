26f1826df63f72909389d5de18bda2c3ccfee163
joaolucascomar <141441384+joaolucascomar@users.noreply.github.com>
2024-10-25 08:47:27 -0300

    asd

--- main.js
+++ main.js
@@ -21,5 +21,7 @@ function criaCartao(categoria, pergunta, resposta) {
         cartao.classList.toggle('active', respostaEstaVisivel);
     }
 
+    cartao.addEventListener('click', viraCartao)
+
     container.appendChild(cartao)
 }
