ac2347bb2982d3ff3b56261c02d760934b9982ab
joaolucascomar <141441384+joaolucascomar@users.noreply.github.com>
2024-10-25 08:45:45 -0300

    sad

--- main.js
+++ main.js
@@ -13,5 +13,13 @@ function criaCartao(categoria, pergunta, resposta) {
             </div>
         </div>
     `;
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+
     container.appendChild(cartao)
 }
