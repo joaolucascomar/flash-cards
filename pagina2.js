fbd89eae3eef6468d78d61e422db4509f395ff9c
joaolucascomar <141441384+joaolucascomar@users.noreply.github.com>
2024-11-01 08:23:39 -0300

    asd

--- pagina2.js
+++ pagina2.js
@@ -17,8 +17,8 @@ function criaCartao(categoria, pergunta, resposta) {
     container.appendChild(cartao)
 }
 
-function mostrarResposta() {
-    const resposta = document.querySelector('.resposta-cartao')
+function mostrarResposta(card) {
+    const resposta = card.querySelector('.resposta-cartao')
 
     if (resposta.style.display === 'none') {
         resposta.style.display = 'block'
